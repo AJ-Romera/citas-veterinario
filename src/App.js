6f931daf14a11762b435793af77ed2b2f8c1e479
AJ-Romera <ajromeraalfonso@gmail.com>
2021-02-22 12:30:39 +0100

    CHNG: Divido App en Dos Columnas Skeleton y Creo Componente Formulario

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
     return (
         <div className='App'>
             <h1>Administrador de Pacientes</h1>
+
+            <div className='container'>
+                <div className='row'>
+                    <div className='one-half column'>
+                        <Formulario />
+                    </div>
+                    <div className='one-half column'>2</div>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/components/Formulario.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Formulario() {
+    return (
+        <div className='formulario'>
+            <h1>Desde Formulario</h1>
+        </div>
+    );
+}
+
+export default Formulario;
