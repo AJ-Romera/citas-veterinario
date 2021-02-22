f250882c570334e083917ca20536de9a9d669366
AJ-Romera <ajromeraalfonso@gmail.com>
2021-02-22 13:07:19 +0100

    CHNG: Crear el State para las Citas

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Formulario() {
+    // Crear State de las citas
+    const [cita, setCita] = useState({
+        mascota: '',
+        propietario: '',
+        fecha: '',
+        hora: '',
+        sintomas: '',
+    });
+
+    // Función que se ejecuta cada vez que un usuario escribe en un input
+    const handleChange = () => {
+        console.log('No escribas encima mia hijo de p....');
+    };
+
     return (
         <div className='formulario'>
             <h2>Crear Cita</h2>
@@ -12,6 +26,7 @@ function Formulario() {
                     name='mascota'
                     className='u-full-width'
                     placeholder='Nombre Mascota'
+                    onChange={handleChange}
                 />
 
                 <label>Nombre Dueño</label>
@@ -20,16 +35,31 @@ function Formulario() {
                     name='propietario'
                     className='u-full-width'
                     placeholder='Nombre Dueño de la Mascota'
+                    onChange={handleChange}
                 />
 
                 <label>Fecha</label>
-                <input type='date' name='fecha' className='u-full-width' />
+                <input
+                    type='date'
+                    name='fecha'
+                    className='u-full-width'
+                    onChange={handleChange}
+                />
 
                 <label>Hora</label>
-                <input type='time' name='hora' className='u-full-width' />
+                <input
+                    type='time'
+                    name='hora'
+                    className='u-full-width'
+                    onChange={handleChange}
+                />
 
                 <label>Síntomas</label>
-                <textarea className='u-full-width' name='sintomas'></textarea>
+                <textarea
+                    name='sintomas'
+                    className='u-full-width'
+                    onChange={handleChange}
+                ></textarea>
 
                 <button type='submit' className='u-full-width button-primary'>
                     Agregar Cita
