fb42a7ce96cd5eb7488764dde31ba8d3cc4e5b20
AJ-Romera <ajromeraalfonso@gmail.com>
2021-02-22 12:50:09 +0100

    CHNG: Hacer los Campos del Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,7 +3,38 @@ import React from 'react';
 function Formulario() {
     return (
         <div className='formulario'>
-            <h1>Desde Formulario</h1>
+            <h2>Crear Cita</h2>
+
+            <form>
+                <label>Nombre Mascota</label>
+                <input
+                    type='text'
+                    name='mascota'
+                    className='u-full-width'
+                    placeholder='Nombre Mascota'
+                />
+
+                <label>Nombre Dueño</label>
+                <input
+                    type='text'
+                    name='propietario'
+                    className='u-full-width'
+                    placeholder='Nombre Dueño de la Mascota'
+                />
+
+                <label>Fecha</label>
+                <input type='date' name='fecha' className='u-full-width' />
+
+                <label>Hora</label>
+                <input type='time' name='hora' className='u-full-width' />
+
+                <label>Síntomas</label>
+                <textarea className='u-full-width' name='sintomas'></textarea>
+
+                <button type='submit' className='u-full-width button-primary'>
+                    Agregar Cita
+                </button>
+            </form>
         </div>
     );
 }
