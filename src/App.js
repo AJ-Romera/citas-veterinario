4996442a55e9192496205ffebdff3a23ae681632
AJ-Romera <ajromeraalfonso@gmail.com>
2021-02-23 14:11:19 +0100

    CHNG: Cambiar Título del Administrador de Citas según si hay Citas o no

--- src/App.js
+++ src/App.js
@@ -17,6 +17,9 @@ function App() {
         setCitas(nuevasCitas);
     };
 
+    // Mensaje condicional según si no hay citas o si las hay
+    const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
+
     return (
         <div className='App'>
             <h1>Administrador de Pacientes</h1>
@@ -27,7 +30,7 @@ function App() {
                         <Formulario crearCita={crearCita} />
                     </div>
                     <div className='one-half column'>
-                        <h2>Administra tus citas</h2>
+                        <h2>{titulo}</h2>
                         {citas.map((cita) => (
                             <Cita
                                 key={cita.id}
